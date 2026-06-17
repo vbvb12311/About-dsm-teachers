fe090fef52fdab5f6de020d4543545587162a37a
kim seung su <20261404@dsm.hs.kr>
2026-06-17 11:35:33 +0900

    시험
    아무내용

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-window.onload = function () {
-  if (window.lucide) window.lucide.createIcons();
-};
+function showPage(pageName) {
+  var pages = document.getElementsByClassName("page");
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].style.display = "none";
+  }
+  document.getElementById(pageName).style.display = "block";
+}
